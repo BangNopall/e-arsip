161cf49719fe08de61b64f9c92a4f9a008c59261
davapasya <davapasyamaulana@gmail.com>
2023-12-23 13:09:06 +0700

    DASHBOARD.BLADE & JS IS DONE SIDE BAR DONE

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,3 @@
-<footer class="text-stone-900 text-sm text-left ml-4 mb-5 absolute bottom-0 left-0 w-full bg-gray-50">
+<!-- <footer class="text-stone-900 text-sm text-left ml-4 mb-5 absolute bottom-0 left-0 w-full bg-gray-50">
     &copy; 2023 Sip Arsip. All rights reserved.
-</footer>
+</footer> -->
